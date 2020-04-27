be0be78da9e1edd48c98adf43a9a98f22848e5fc
hongml000 <286995954@qq.com>
2020-04-27 18:18:37 +0800

    update notes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoList from "./todoList";
+import TodoList from "./todolist";
 // 引入antd样式
 import "antd/dist/antd.css";
 import "./assets/iconfonts/iconfont.css"
 import './index.css';
 
+import { Provider } from 'react-redux'
+import store from './store'
 
-ReactDOM.render(<TodoList />,document.getElementById('root'))
+ReactDOM.render(
+  // 将store以属性形式提供给组件的props
+  <Provider store={store}>
+    <TodoList />
+  </Provider>,
+  document.getElementById('root')
+)
--- src/todolist.js
+++ src/todolist.js
@@ -1,10 +1,89 @@
-import React, { Component } from 'react'
-class TodoList extends Component {
-  render() {
+// import React, { Component } from 'react'
+import React from 'react'
+
+// import store from './store'
+import { connect } from 'react-redux'
+
+// TodoList只是一个UI组件，可优化成无状态组件
+const TodoList = (props) => {
+  const { inputValue, changeInputValue, addItem, deleteItem, list } = props
     return (
-      <div>hello world</div>
+      <div>
+        <div>
+          {/* <input value={ this.props.inputValue } onChange={ this.props.changeInputValue } type="text"/> */}
+          <input value={ inputValue } onChange={ changeInputValue } type="text"/>
+          <button onClick={ addItem }>提交</button>
+        </div>
+        <ul>
+          { list.map((item,index) => {
+              return (
+                // 使用箭头函数来传递带参数的函数
+                <li key={ index } onClick={ () => { deleteItem(index) } }>{ item }</li>
+              )
+            })
+          }
+        </ul>
+      </div>
     )
+}
+
+
+// class TodoList extends Component {
+//   render() {
+//     // 优化
+//     const { inputValue, changeInputValue, addItem, deleteItem } = this.props
+//     return (
+//       <div>
+//         <div>
+//           {/* <input value={ this.props.inputValue } onChange={ this.props.changeInputValue } type="text"/> */}
+//           <input value={ inputValue } onChange={ changeInputValue } type="text"/>
+//           <button onClick={ addItem }>提交</button>
+//         </div>
+//         <ul>
+//           { this.props.list.map((item,index) => {
+//               return (
+//                 // 使用箭头函数来传递带参数的函数
+//                 <li key={ index } onClick={ () => { deleteItem(index) } }>{ item }</li>
+//               )
+//             })
+//           }
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+
+const mapStateToProps = (state) => {
+  return {
+    inputValue: state.inputValue,
+    list: state.list
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    changeInputValue(e) {
+      console.log(e.target.value)
+      const action = {
+        type: 'change_input_value',
+        value: e.target.value
+      }
+      dispatch(action)
+    },
+    addItem() {
+      const action = {
+        type: 'add_item'
+      }
+      dispatch(action)
+    },
+    deleteItem(index) {
+      const action = {
+        type: 'delete_item',
+        index
+      }
+      dispatch(action)
+    }
   }
 }
 
-export default TodoList;
+
+export default connect(mapStateToProps,mapDispatchToProps)(TodoList);
